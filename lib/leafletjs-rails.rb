73558ff8cb43d5521d95a55b94535eb7b7e0de8b
Tom de Grunt <tom@degrunt.nl>
2015-03-16 22:25:18 +0100

    Add paths

--- lib/leafletjs-rails.rb
+++ lib/leafletjs-rails.rb
@@ -5,6 +5,9 @@ module Leafletjs
   module Rails
     class Engine < ::Rails::Engine
       initializer 'leafletjs-rails.precompile' do |app|
+        %w(stylesheets javascripts images).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
         app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png spritesheet.png spritesheet-2x.png)
       end
 

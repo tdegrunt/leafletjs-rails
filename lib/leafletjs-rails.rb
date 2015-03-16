f39fc8c1b73f834ccde54961319688d183513f66
Tom de Grunt <tom@degrunt.nl>
2015-03-16 21:58:40 +0100

    Add in leaflet.draw

--- lib/leafletjs-rails.rb
+++ lib/leafletjs-rails.rb
@@ -5,7 +5,7 @@ module Leafletjs
   module Rails
     class Engine < ::Rails::Engine
       initializer 'leafletjs-rails.precompile' do |app|
-        app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png)
+        app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png spritesheet.png spritesheet-2x.png)
       end
 
       initializer 'leafletjs-rails.helpers' do
